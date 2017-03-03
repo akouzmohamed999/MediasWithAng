a262072e9c1230aaf204aa5a9b77375fb1d0741a
akouz mohamed <akouz.mohamed999@gmail.com>
2017-03-03 10:12:36 +0000

    finnaly fixing the form and usung the model one

--- src/app/media-item-form.component.ts
+++ src/app/media-item-form.component.ts
@@ -1,12 +1,27 @@
 
 import {Component} from '@angular/core';
+import {FormGroup,FormBuilder,Validators} from '@angular/forms';
 @Component({
 selector :'media-item-form',
 templateUrl : './media-item-form.component.html',
 styleUrls : ['./media-item-form.component.css']
 })
 export class MediaItemFormComponent{
-    onSubmit(MediaItem){
-        console.log(MediaItem);
+
+    form ; FormGroup;
+
+    constructor(fb : FormBuilder){
+        this.form = fb.group({
+            'medium' : ['movies',Validators.compose([
+                Validators.required,
+                Validators.pattern('[\\w\\-\\s\\/]+')
+                       ])],
+            'name'  : [],
+            'category' : [],
+            'year' : []
+        })
+    }
+    onSubmit(mediaItem){
+        console.log(mediaItem);
     }
 }
