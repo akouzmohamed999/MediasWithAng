49aacd9ade656eded1a5719cde22cd6771c2e267
akouz mohamed <akouz.mohamed999@gmail.com>
2017-03-06 15:36:15 +0000

    adding interface

---
+++ src/app/media-item.interface.ts
@@ -0,0 +1,12 @@
+
+export interface MediaItem{
+
+            id: number,
+            name: String,
+            medium: String,
+            category: String,
+            year: Date,
+            watchedOn: number,
+            isFavorite: boolean
+}
+
